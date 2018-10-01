78c44ba0645e270bad9300b0496bd67d56ff96c9
brianisadog <s254901282002@gmail.com>
2018-10-01 16:52:25 -0700

    is toeplitz matrix follow up

--- is_toeplitz_matrix.py
+++ is_toeplitz_matrix.py
@@ -13,11 +13,66 @@ def is_toeplitz_matrix(matrix):
     return True
 
 
+def is_toeplitz_matrix_1(matrix, k):
+    """
+    Follow up: What if the matrix is so large that you can only load up a partial row into the memory at once?
+
+    My solution:
+    Load k rows a time and compare only between these k rows, overlap the first row with the previous last row.
+    len(matrix) >= k >= 2, k is the maximum row number that our memory can handle for loading partial matrix.
+    """
+    def get_k_row_start_from_i(m, i, k):
+        return m[i:i + k]
+
+    # step k - 1 since we are overlapping the first row with the previous last row.
+    for i in range(0, len(matrix) - 1, k - 1):
+        m = get_k_row_start_from_i(matrix, i, k)
+        if not is_toeplitz_matrix(m):
+            return False
+    return True
+
+
+# True
 test_l = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
 print(is_toeplitz_matrix(test_l))
+print(is_toeplitz_matrix_1(test_l, 2))
 
+print()
+
+# False
 test_l = [[1, 2], [2, 2]]
 print(is_toeplitz_matrix(test_l))
+print(is_toeplitz_matrix_1(test_l, 2))
 
+print()
+
+# True
 test_l = [[18], [66]]
 print(is_toeplitz_matrix(test_l))
+print(is_toeplitz_matrix_1(test_l, 2))
+
+print()
+
+# True
+test_l = [
+    [1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5],
+    [9, 0, 1, 2, 3, 4],
+    [8, 9, 0, 1, 2, 3],
+    [7, 8, 9, 0, 1, 2],
+]
+print(is_toeplitz_matrix(test_l))
+print(is_toeplitz_matrix_1(test_l, 4))
+
+print()
+
+# False
+test_l = [
+    [1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5],
+    [9, 0, 1, 4, 3, 4],
+    [8, 9, 0, 1, 2, 3],
+    [7, 8, 9, 0, 1, 2],
+]
+print(is_toeplitz_matrix(test_l))
+print(is_toeplitz_matrix_1(test_l, 3))
